040fd2f1e3c480723a28ea1263e7b5c23d0f65c6
0jun <an0yjun@gmail.com>
2025-09-01 16:52:21 +0900

    patch cors 추가

--- src/main/java/com/aisolutionvoice/config/security/SecurityConfig.java
+++ src/main/java/com/aisolutionvoice/config/security/SecurityConfig.java
@@ -67,7 +67,7 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
         config.setAllowedOriginPatterns(List.of("http://localhost:3000"));
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        config.setAllowedMethods(List.of("GET", "POST","PATCH", "PUT", "DELETE"));
         config.setAllowedHeaders(List.of("*"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
