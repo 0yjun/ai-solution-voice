2f01a16dac1d7d73eb64105cb9dcc7ecf75914ce
0jun <an0yjun@gmail.com>
2025-09-03 18:27:55 +0900

    /post /boards/boardid/posts/postsid 변경

--- src/main/java/com/aisolutionvoice/api/post/controller/PostController.java
+++ src/main/java/com/aisolutionvoice/api/post/controller/PostController.java
@@ -18,7 +18,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/posts")
+@RequestMapping("/api/boards/{boardId}/posts")
 @RequiredArgsConstructor
 @Tag(name = "Post", description = "게시글 제공 API")
 @Slf4j
@@ -27,6 +27,7 @@ public class PostController {
 
     @GetMapping
     public ResponseEntity<BoardPageDto> getPost(
+            @PathVariable Long boardId,
             PostSearchRequestDto searchRequest,
             Pageable pageable,
             @AuthenticationPrincipal CustomMemberDetails customMemberDetails
@@ -35,12 +36,14 @@ public class PostController {
         if(Boolean.TRUE.equals(searchRequest.getOnlyMyPosts())){
             memberId = customMemberDetails.getUserId();
         }
+        searchRequest.setBoardId(boardId);
         BoardPageDto postSummaryDtoPage = postService.getSearchAndNotice(searchRequest, memberId, pageable);
         return ResponseEntity.ok(postSummaryDtoPage);
     }
 
     @GetMapping("/stat")
     public ResponseEntity<PostStatDto> getPostStat(
+            @PathVariable Long boardId,
             PostSearchRequestDto searchRequest,
             @AuthenticationPrincipal CustomMemberDetails customMemberDetails
     ) {
@@ -48,12 +51,13 @@ public class PostController {
         if(Boolean.TRUE.equals(searchRequest.getOnlyMyPosts())){
             memberId = customMemberDetails.getUserId();
         }
+        searchRequest.setBoardId(boardId);
         PostStatDto stats = postService.getPostStats(searchRequest, memberId);
         return ResponseEntity.ok(stats);
     }
 
     @GetMapping("/{postId}")
-    public ResponseEntity<PostDetailDto> getPostId(@PathVariable Long postId) {
+    public ResponseEntity<PostDetailDto> getPostId(@PathVariable Long boardId, @PathVariable Long postId) {
         PostDetailDto postDetailDto = postService.getByPostId(postId);
 
         return ResponseEntity.ok(postDetailDto);
@@ -61,8 +65,8 @@ public class PostController {
 
     @GetMapping("/me/check")
     public ResponseEntity<?> checkMe(
-            @AuthenticationPrincipal CustomMemberDetails customMemberDetails,
-            @RequestParam Long boardId
+            @PathVariable Long boardId,
+            @AuthenticationPrincipal CustomMemberDetails customMemberDetails
     ) {
         Integer memberId = customMemberDetails.getUserId();
         return postService.findMyPostId(memberId, boardId)
@@ -72,18 +76,20 @@ public class PostController {
 
     @PostMapping( consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<?> uploadPostWithVoice(
+            @PathVariable Long boardId,
             @RequestPart("postCreateDto") @Validated PostCreateDto dto,
             @RequestParam Map<String, MultipartFile> files,
             @AuthenticationPrincipal CustomMemberDetails customMemberDetails
     ) {
         Integer memberId = customMemberDetails.getUserId();
-        postService.createPostWithVoiceFiles(dto, files, memberId, 1L);
+        postService.createPostWithVoiceFiles(dto, files, memberId, boardId);
 
         return ResponseEntity.ok(Map.of("data","ok"));
     }
 
     @PutMapping( consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<?> editPostWithVoice(
+            @PathVariable Long boardId,
             @RequestPart("PostUpdateDto") @Validated PostUpdateDto dto,
             @RequestParam Map<String, MultipartFile> files,
             @AuthenticationPrincipal CustomMemberDetails customMemberDetails
@@ -94,9 +100,10 @@ public class PostController {
         return ResponseEntity.ok(Map.of("data","ok"));
     }
 
-    @PatchMapping("/{postId:\\d+}/check")
+    @PatchMapping("/{postId:[0-9]+}/check")
     @PreAuthorize("hasRole(Role.ADMIN)")
     public ResponseEntity<Map<String, Boolean>> setChecked(
+            @PathVariable Long boardId,
             @PathVariable Long postId,
             @RequestBody boolean isChecked
     ) {
